6a255d80527086f26655c2bd2ee663e1d2927a0a
Tsubasa <scientistjishan@gmail.com>
2023-05-30 20:58:01 +0530

    This file contains all the different types of tokens.

---
+++ interpreter/token_type.py
@@ -0,0 +1,51 @@
+from enum import Enum
+
+
+class TokenType(Enum):
+    # Single character tokens
+    LEFT_PAREN = "("
+    RIGHT_PAREN = ")"
+    LEFT_BRACE = "{"
+    RIGHT_BRACE = "}"
+    COMMA = ","
+    DOT = "."
+    MINUS = "-"
+    PLUS = "+"
+    SEMICOLON = ";"
+    SLASH = "/"
+    STAR = "*"
+
+    # One or two character tokens
+    BANG = "!"
+    BANG_EQUAL = "!="
+    EQUAL = "="
+    EQUAL_EQUAL = "=="
+    GREATER = ">"
+    GREATER_EQUAL = ">="
+    LESS = "<"
+    LESS_EQUAL = "<="
+
+    # Literals
+    IDENTIFIER = "IDENTIFIER"
+    STRING = "STRING"
+    NUMBER = "NUMBER"
+
+    # Keywords
+    AND = "&&"
+    CLASS = "class"
+    ELSE = "else"
+    FALSE = "false"
+    FUN = "fun"
+    FOR = "for"
+    IF = "if"
+    NIL = "nil"
+    OR = "||"
+    PRINT = "print"
+    RETURN = "return"
+    SUPER = "super"
+    THIS = "this"
+    TRUE = "true"
+    VAR = "var"
+    WHILE = "while"
+
+    EOF = "EOF"

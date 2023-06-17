00ea141af156abac92b8a9e1c92e4b9fab2cb375
Tsubasa <scientistjishan@gmail.com>
2023-06-17 20:08:05 +0530

    Added support for scanner, parser and interpreter.

--- interpreter/lox.py
+++ interpreter/lox.py
@@ -1,14 +1,25 @@
 import sys
 from typing import Optional, ClassVar, cast
 from .token import Token
+from .parser import Parser
 from .scanner import Scanner
+from .token_type import TokenType
+from .expr import Expr
+from tools.ast_printer import AstPrinter
+from .parser import ParseError
+from .runtime_error import RuntimeError
+from .interpreter import Interpreter
 
 
 class Lox:
     had_error: ClassVar[bool] = False
+    had_runtime_error: ClassVar[bool] = False
+    _interpreter: ClassVar[Interpreter]
 
     @classmethod
     def execute(cls, file: Optional[str] = None) -> None:
+        cls._interpreter = Interpreter(cast(Lox, cls))
+
         if file:
             cls.run_file(file)
         else:
@@ -22,6 +33,8 @@ class Lox:
             # Indicate an error in the exit code.
             if cls.had_error:
                 sys.exit(65)
+            if cls.had_runtime_error:
+                sys.exit(70)
 
     @classmethod
     def run_prompt(cls) -> None:
@@ -29,22 +42,46 @@ class Lox:
             try:
                 line: str = input("> ")
                 cls.run(line)
-                cls.had_error = False
             except KeyboardInterrupt:
                 break
+            except ParseError:
+                ...
+            except RuntimeError:
+                ...
+            finally:
+                cls.had_error = False
+                cls.had_runtime_error = False
 
     @classmethod
     def run(cls, source: str) -> None:
-        scanner: Scanner = Scanner(source, cast(cls, Lox))
+        scanner: Scanner = Scanner(source, cast(Lox, cls))
         tokens: list[Token] = scanner.scan_tokens()
+        parser: Parser = Parser(tokens, cast(Lox, cls))
+        expression: Expr = parser.parse()
 
-        for token in tokens:
-            print(token)
+        # Stop if there was a syntax error.
+        if cls.had_error:
+            return
+
+        cls._interpreter.interpret(expression)
+        print(AstPrinter().print(expression))
 
     @classmethod
-    def error(cls, line: int, message: str) -> None:
+    def error_on_line(cls, line: int, message: str) -> None:
         cls.report(line, "", message)
+
+    @classmethod
+    def error_on_token(cls, token: Token, message: str) -> None:
+        if token.type == TokenType.EOF:
+            cls.report(token.line, " at end", message)
+        else:
+            cls.report(token.line, f" at '{token.lexeme}'", message)
 
     @staticmethod
     def report(line: int, where: str, message: str) -> None:
         print(f"[line {line}] Error{where}: {message}")
+
+    @staticmethod
+    def runtime_error(error: RuntimeError) -> None:
+        print(f"[line {error.token.line}] {error.message}")
+        Lox.had_runtime_error = True

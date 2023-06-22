7d459e7d9a122267907a5094f3107d7dfa287cc8
Tsubasa <scientistjishan@gmail.com>
2023-06-22 16:54:57 +0530

    Added support for functions.

---
+++ interpreter/function.py
@@ -0,0 +1,40 @@
+from .callable import Callable
+from .expr import Function as FunctionExpr
+from typing import Final, TYPE_CHECKING, Optional
+from .environment import Environment
+from .exceptions import Return
+
+if TYPE_CHECKING:
+    from .interpreter import Interpreter
+
+
+class Function(Callable):
+    def __init__(
+        self: "Function",
+        name: Optional[str],
+        declaration: FunctionExpr,
+        closure: Environment,
+    ):
+        self._name: Final[Optional[str]] = name
+        self._closure: Final[Environment] = closure
+        self._declaration: Final[FunctionExpr] = declaration
+
+    def __str__(self: "Function") -> str:
+        if self._name is None:
+            return "<fn>"
+        return f"<fn {self._name}>"
+
+    def arity(self: "Function") -> int:
+        return len(self._declaration.params)
+
+    def call(
+        self: "Function", interpreter: "Interpreter", arguments: list[object]
+    ) -> object:
+        environment: Environment = Environment(self._closure)
+        for i in range(len(self._declaration.params)):
+            environment.define(self._declaration.params[i].lexeme, arguments[i])
+
+        try:
+            interpreter.execute_block(self._declaration.body, environment)
+        except Return as r:
+            return r.value

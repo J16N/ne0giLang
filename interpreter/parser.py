cfb9c0fc7600ced7a15e53cd5df577a2a499f318
Tsubasa <scientistjishan@gmail.com>
2023-06-22 17:00:07 +0530

    Added support for various expressions and statements.

--- interpreter/parser.py
+++ interpreter/parser.py
@@ -1,7 +1,34 @@
-from typing import Final, TYPE_CHECKING
+from typing import Final, TYPE_CHECKING, Optional
 from .token import Token
 from .token_type import TokenType
-from .expr import Expr, Binary, Comma, Grouping, Literal, Ternary, Unary
+from .expr import (
+    Assign,
+    Binary,
+    Call,
+    Comma,
+    Expr,
+    Function as FunctionExpr,
+    Grouping,
+    Literal,
+    Logical,
+    Ternary,
+    Unary,
+    Variable,
+)
+from .stmt import (
+    Block,
+    Break,
+    Continue,
+    Expression,
+    For,
+    Function as FunctionStmt,
+    If,
+    MultiVar,
+    Return,
+    Stmt,
+    Var,
+    While,
+)
 
 if TYPE_CHECKING:
     from .lox import Lox
@@ -16,31 +43,241 @@ class Parser:
         self._tokens: Final[list[Token]] = tokens
         self._current: int = 0
         self._agent = agent
+        self._loop_depth: int = 0
 
-    def parse(self: "Parser") -> Expr:
-        return self._expression()
+    def parse(self: "Parser") -> list[Optional[Stmt]]:
+        statements: list[Optional[Stmt]] = []
+
+        while not self._is_at_end():
+            statements.append(self._declaration())
+
+        return statements
+
+    def _declaration(self: "Parser") -> Optional[Stmt]:
+        try:
+            if self._match(TokenType.VAR):
+                return self._multi_var_declaration()
+            return self._statement()
+
+        except ParseError:
+            self._synchronize()
+
+    def _multi_var_declaration(self: "Parser") -> Stmt:
+        multi_var: MultiVar = MultiVar()
+        multi_var.add(self._var_declaration())
+
+        while self._match(TokenType.COMMA):
+            multi_var.add(self._var_declaration())
+
+        self._consume(TokenType.SEMICOLON, "Expected ';' after variable declaration.")
+        return multi_var
+
+    def _var_declaration(self: "Parser") -> Var:
+        name: Token = self._consume(TokenType.IDENTIFIER, "Expected variable name.")
+
+        initializer: Optional[Expr] = None
+        if self._match(TokenType.EQUAL):
+            initializer = self._assignment()
+
+        return Var(name, initializer)
+
+    def _statement(self: "Parser") -> Optional[Stmt]:
+        if self._match(TokenType.BREAK):
+            return self._break_statement()
+
+        if self._match(TokenType.CONTINUE):
+            return self._continue_statement()
+
+        if self._match(TokenType.FOR):
+            return self._for_statement()
+
+        if self._match(TokenType.FN):
+            return self._function("function")
+
+        if self._match(TokenType.IF):
+            return self._if_statement()
+
+        if self._match(TokenType.LEFT_BRACE):
+            return Block(self._block())
+
+        if self._match(TokenType.RETURN):
+            return self._return_statement()
+
+        if self._match(TokenType.SEMICOLON):
+            return
+
+        if self._match(TokenType.WHILE):
+            return self._while_statement()
+
+        return self._expression_statement()
+
+    def _break_statement(self: "Parser") -> Stmt:
+        if self._loop_depth == 0:
+            raise ParseError("Cannot break outside of a loop.")
+        self._consume(TokenType.SEMICOLON, "Expected ';' after 'break'.")
+        return Break()
+
+    def _continue_statement(self: "Parser") -> Stmt:
+        if self._loop_depth == 0:
+            raise ParseError("Cannot continue outside of a loop.")
+        self._consume(TokenType.SEMICOLON, "Expected ';' after 'continue'.")
+        return Continue()
+
+    def _for_statement(self: "Parser") -> Stmt:
+        self._consume(TokenType.LEFT_PAREN, "Expected '(' after 'for'.")
+        initializer: Optional[Stmt] = None
+        if self._match(TokenType.VAR):
+            initializer = self._multi_var_declaration()
+        elif not self._match(TokenType.SEMICOLON):
+            initializer = self._expression_statement()
+
+        condition: Optional[Expr] = None
+        if not self._check(TokenType.SEMICOLON):
+            condition = self._expression()
+        self._consume(TokenType.SEMICOLON, "Expected ';' after loop condition.")
+
+        increment: Optional[Expr] = None
+        if not self._check(TokenType.RIGHT_PAREN):
+            increment = self._expression()
+        self._consume(TokenType.RIGHT_PAREN, "Expected ')' after for clause.")
+
+        try:
+            self._loop_depth += 1
+            body: Optional[Stmt] = self._statement()
+            return Block([For(initializer, condition, increment, body)])
+
+        finally:
+            self._loop_depth -= 1
+
+    def _function(self: "Parser", kind: str) -> Stmt:
+        name: Token = self._consume(TokenType.IDENTIFIER, f"Expected {kind} name.")
+        return FunctionStmt(name, self._function_body(kind))
+
+    def _function_body(self: "Parser", kind: str) -> FunctionExpr:
+        self._consume(TokenType.LEFT_PAREN, f"Expected '(' after {kind} name.")
+        parameters: list[Token] = []
+
+        if not self._check(TokenType.RIGHT_PAREN):
+            while True:
+                if len(parameters) >= 255:
+                    self._error(self._peek(), "Cannot have more than 255 parameters.")
+
+                parameters.append(
+                    self._consume(TokenType.IDENTIFIER, "Expected parameter name.")
+                )
+
+                if not self._match(TokenType.COMMA):
+                    break
+
+        self._consume(TokenType.RIGHT_PAREN, "Expected ')' after parameters.")
+        self._consume(TokenType.LEFT_BRACE, f"Expected '{{' before {kind} body.")
+        body: list[Optional[Stmt]] = self._block()
+        return FunctionExpr(parameters, body)
+
+    def _if_statement(self: "Parser") -> Stmt:
+        self._consume(TokenType.LEFT_PAREN, "Expected '(' after 'if'.")
+        condition: Expr = self._expression()
+        self._consume(TokenType.RIGHT_PAREN, "Expected ')' after if condition.")
+
+        then_branch: Optional[Stmt] = self._statement()
+        else_branch: Optional[Stmt] = None
+        if self._match(TokenType.ELSE):
+            else_branch = self._statement()
+
+        return If(condition, then_branch, else_branch)
+
+    def _block(self: "Parser") -> list[Optional[Stmt]]:
+        statements: list[Optional[Stmt]] = []
+
+        while not self._check(TokenType.RIGHT_BRACE) and not self._is_at_end():
+            statement: Optional[Stmt] = self._declaration()
+            statements.append(statement)
+
+        self._consume(TokenType.RIGHT_BRACE, "Expected '}' after block.")
+        return statements
+
+    def _return_statement(self: "Parser") -> Stmt:
+        keyword: Token = self._previous()
+        value: Optional[Expr] = None
+        if not self._check(TokenType.SEMICOLON):
+            value = self._expression()
+        self._consume(TokenType.SEMICOLON, "Expected ';' after return value.")
+        return Return(keyword, value)
+
+    def _while_statement(self: "Parser") -> Stmt:
+        self._consume(TokenType.LEFT_PAREN, "Expected '(' after 'while'.")
+        condition: Expr = self._expression()
+        self._consume(TokenType.RIGHT_PAREN, "Expected ')' after condition.")
+
+        try:
+            self._loop_depth += 1
+            body: Optional[Stmt] = self._statement()
+            return While(condition, body)
+
+        finally:
+            self._loop_depth -= 1
+
+    def _expression_statement(self: "Parser") -> Stmt:
+        expr: Expr = self._expression()
+        self._consume(TokenType.SEMICOLON, "Expected ';' after expression.")
+        return Expression(expr)
 
     def _expression(self: "Parser") -> Expr:
         return self._comma_expression()
 
     def _comma_expression(self: "Parser") -> Expr:
-        expr: Expr = self._ternary()
+        expr: Expr = self._assignment()
 
         while self._match(TokenType.COMMA):
             operator: Token = self._previous()
-            right_expr: Expr = self._ternary()
+            right_expr: Expr = self._assignment()
             expr = Comma(expr, operator, right_expr)
 
         return expr
 
+    def _assignment(self: "Parser") -> Expr:
+        expr: Expr = self._ternary()
+
+        if self._match(TokenType.EQUAL):
+            equals: Token = self._previous()
+            value: Expr = self._assignment()
+
+            if isinstance(expr, Variable):
+                name: Token = expr.name
+                return Assign(name, value)
+
+            self._error(equals, "Invalid assignment target.")
+
+        return expr
+
     def _ternary(self: "Parser") -> Expr:
-        expr: Expr = self._equality()
+        expr: Expr = self._or()
 
         while self._match(TokenType.QUESTION):
-            then_branch: Expr = self._ternary()
+            then_branch: Expr = self._assignment()
             self._consume(TokenType.COLON, "Expected ':' after expression.")
-            else_branch: Expr = self._ternary()
+            else_branch: Expr = self._assignment()
             expr = Ternary(expr, then_branch, else_branch)
+
+        return expr
+
+    def _or(self: "Parser") -> Expr:
+        expr: Expr = self._and()
+
+        while self._match(TokenType.OR):
+            operator: Token = self._previous()
+            right: Expr = self._and()
+            expr = Logical(expr, operator, right)
+
+        return expr
+
+    def _and(self: "Parser") -> Expr:
+        expr: Expr = self._equality()
+
+        while self._match(TokenType.AND):
+            operator: Token = self._previous()
+            right: Expr = self._equality()
+            expr = Logical(expr, operator, right)
 
         return expr
 
@@ -95,23 +332,56 @@ class Parser:
             right: Expr = self._unary()
             return Unary(operator, right)
 
-        return self._primary()
+        return self._call()
+
+    def _call(self: "Parser") -> Expr:
+        expr: Expr = self._primary()
+
+        while True:
+            if not self._match(TokenType.LEFT_PAREN):
+                break
+            expr = self._finish_call(expr)
+
+        return expr
+
+    def _finish_call(self: "Parser", callee: Expr) -> Expr:
+        arguments: list[Expr] = []
+        if not self._check(TokenType.RIGHT_PAREN):
+            while True:
+                if len(arguments) >= 255:
+                    self._error(self._peek(), "Cannot have more than 255 arguments.")
+                arguments.append(self._expression())
+                if not self._match(TokenType.COMMA):
+                    break
+
+        paren: Token = self._consume(
+            TokenType.RIGHT_PAREN, "Expected ')' after arguments."
+        )
+        return Call(callee, paren, arguments)
 
     def _primary(self: "Parser") -> Expr:
         if self._match(TokenType.FALSE):
             return Literal(False)
-        if self._match(TokenType.TRUE):
-            return Literal(True)
+
+        if self._match(TokenType.FN):
+            return self._function_body("function")
+
+        if self._match(TokenType.IDENTIFIER):
+            return Variable(self._previous())
+
+        if self._match(TokenType.LEFT_PAREN):
+            expr: Expr = self._expression()
+            self._consume(TokenType.RIGHT_PAREN, "Expected ')' after expression.")
+            return Grouping(expr)
+
         if self._match(TokenType.NIL):
             return Literal(None)
 
         if self._match(TokenType.NUMBER, TokenType.STRING):
             return Literal(self._previous().literal)
 
-        if self._match(TokenType.LEFT_PAREN):
-            expr: Expr = self._expression()
-            self._consume(TokenType.RIGHT_PAREN, "Expected ')' after expression.")
-            return Grouping(expr)
+        if self._match(TokenType.TRUE):
+            return Literal(True)
 
         raise self._error(self._peek(), "Expected expression.")
 
@@ -139,7 +409,7 @@ class Parser:
                 return
 
             match self._peek().type:
-                case TokenType.CLASS | TokenType.FUN | TokenType.VAR | TokenType.FOR | TokenType.IF | TokenType.WHILE | TokenType.PRINT | TokenType.RETURN:
+                case TokenType.CLASS | TokenType.FN | TokenType.VAR | TokenType.FOR | TokenType.IF | TokenType.WHILE | TokenType.RETURN:
                     return
 
                 case _:

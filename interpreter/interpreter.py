ecfe54b2cce732261f7bbc2c124618ccbacfbddb
Tsubasa <scientistjishan@gmail.com>
2023-06-22 16:55:58 +0530

    Added support for various expressions and statements.

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,25 +1,55 @@
-from typing import cast, TYPE_CHECKING
+from typing import cast, TYPE_CHECKING, Optional, Final
 from .token import Token
 from .token_type import TokenType
-from .runtime_error import RuntimeError
+from .exceptions import Break, Continue, Return, RuntimeError
 from .expr import (
+    Assign,
     Binary,
+    Call,
     Comma,
     Expr,
+    Function as FunctionExpr,
     Grouping,
     Literal,
+    Logical,
     Ternary,
     Unary,
+    Variable,
     Visitor as ExprVisitor,
 )
+from .stmt import (
+    Block,
+    Expression,
+    For,
+    Function as FunctionStmt,
+    If,
+    MultiVar,
+    Return as ReturnStmt,
+    Stmt,
+    Var,
+    Visitor as StmtVisitor,
+    While,
+)
+from .environment import Environment
+from .types import Uninitialized
+from .callable import Callable
+from .utils import Clock, Print
+from .function import Function
 
 if TYPE_CHECKING:
     from .lox import Lox
 
 
-class Interpreter(ExprVisitor[object]):
-    def __init__(self: "Interpreter", agent: "Lox"):
-        self.agent = agent
+class Interpreter(ExprVisitor[object], StmtVisitor[None]):
+    def __init__(self: "Interpreter", agent: "Lox", repl: bool = False):
+        self._agent: "Lox" = agent
+        self._repl: bool = repl
+        self._print_called: bool = False
+        self.globals: Final[Environment] = Environment()
+        self._environment: Environment = self.globals
+        self._locals: dict[Expr, int] = {}
+        self.globals.define("clock", Clock())
+        self.globals.define("print", Print())
 
     def _check_number_operands(
         self: "Interpreter", operator: Token, left: object, right: object
@@ -38,17 +68,48 @@ class Interpreter(ExprVisitor[object]):
     def _evaluate(self: "Interpreter", expr: Expr) -> object:
         return expr.accept(self)
 
+    def _execute(self: "Interpreter", stmt: Optional[Stmt]) -> None:
+        if not stmt:
+            return
+        stmt.accept(self)
+
     def _is_equal(self: "Interpreter", a: object, b: object) -> bool:
         return a == b
 
     def _is_truthy(self: "Interpreter", obj: object) -> bool:
-        if obj is None:
-            return False
-        if isinstance(obj, bool):
-            return bool(obj)
-        return True
+        return bool(obj)
 
-    def _stringify(self: "Interpreter", obj: object) -> str:
+    def _lookup_variable(self: "Interpreter", name: Token, expr: Expr) -> object:
+        distance: int = self._locals.get(expr, -1)
+        if distance >= 0:
+            return self._environment.get_at(distance, name.lexeme)
+        return self.globals.get(name)
+
+    def execute_block(
+        self: "Interpreter", statements: list[Optional[Stmt]], environment: Environment
+    ) -> None:
+        previous: Environment = self._environment
+        try:
+            self._environment = environment
+
+            for statement in statements:
+                self._execute(statement)
+
+        finally:
+            self._environment = previous
+
+    def interpret(self: "Interpreter", statements: list[Optional[Stmt]]) -> None:
+        try:
+            for statement in statements:
+                self._execute(statement)
+
+        except RuntimeError as e:
+            self._agent.runtime_error(e)
+
+    def resolve(self: "Interpreter", expr: Expr, depth: int) -> None:
+        self._locals[expr] = depth
+
+    def stringify(self: "Interpreter", obj: object) -> str:
         if obj is None:
             return "nil"
 
@@ -58,15 +119,19 @@ class Interpreter(ExprVisitor[object]):
         if isinstance(obj, bool):
             return str(obj).lower()
 
+        if isinstance(obj, str):
+            return f"'{obj}'"
+
         return str(obj)
 
-    def interpret(self: "Interpreter", expr: Expr) -> None:
-        try:
-            value: object = self._evaluate(expr)
-            print(self._stringify(value))
-
-        except RuntimeError as e:
-            self.agent.runtime_error(e)
+    def visit_assign_expr(self: "Interpreter", expr: Assign) -> object:
+        value: object = self._evaluate(expr.value)
+        distance: int = self._locals.get(expr, -1)
+        if distance >= 0:
+            self._environment.assign_at(distance, expr.name, value)
+        else:
+            self.globals.assign(expr.name, value)
+        return value
 
     def visit_binary_expr(self: "Interpreter", expr: Binary) -> object:
         left: object = self._evaluate(expr.left)
@@ -151,15 +216,51 @@ class Interpreter(ExprVisitor[object]):
             case _:
                 ...
 
+    def visit_call_expr(self: "Interpreter", expr: Call) -> object:
+        callee: object = self._evaluate(expr.callee)
+        if isinstance(callee, Print):
+            self._print_called = True
+
+        arguments: list[object] = [
+            self._evaluate(argument) for argument in expr.arguments
+        ]
+
+        if not isinstance(callee, Callable):
+            raise RuntimeError(expr.paren, "Can only call functions and classes.")
+
+        function: Callable = callee
+        if len(arguments) != function.arity():
+            raise RuntimeError(
+                expr.paren,
+                f"Expected {function.arity()} arguments but got {len(arguments)}.",
+            )
+
+        return function.call(self, arguments)
+
     def visit_comma_expr(self: "Interpreter", expr: Comma) -> object:
         self._evaluate(expr.left)
         return self._evaluate(expr.right)
+
+    def visit_function_expr(self: "Interpreter", expr: FunctionExpr) -> object:
+        return Function(None, expr, self._environment)
 
     def visit_grouping_expr(self: "Interpreter", expr: Grouping) -> object:
         return self._evaluate(expr.expression)
 
     def visit_literal_expr(self: "Interpreter", expr: Literal) -> object:
         return expr.value
+
+    def visit_logical_expr(self: "Interpreter", expr: Logical) -> object:
+        left: object = self._evaluate(expr.left)
+
+        if expr.operator.type == TokenType.OR:
+            if self._is_truthy(left):
+                return left
+        else:
+            if not self._is_truthy(left):
+                return left
+
+        return self._evaluate(expr.right)
 
     def visit_ternary_expr(self: "Interpreter", expr: Ternary) -> object:
         condition: object = self._evaluate(expr.condition)
@@ -184,3 +285,74 @@ class Interpreter(ExprVisitor[object]):
                 ...
 
         return None
+
+    def visit_variable_expr(self: "Interpreter", expr: Variable) -> object:
+        return self._lookup_variable(expr.name, expr)
+
+    def visit_block_stmt(self: "Interpreter", stmt: Block) -> None:
+        self.execute_block(stmt.statements, Environment(self._environment))
+
+    def visit_break_stmt(self: "Interpreter") -> None:
+        raise Break()
+
+    def visit_continue_stmt(self: "Interpreter") -> None:
+        raise Continue()
+
+    def visit_expression_stmt(self: "Interpreter", stmt: Expression) -> None:
+        value: object = self._evaluate(stmt.expression)
+        if self._repl and not self._print_called:
+            print(self.stringify(value))
+        self._print_called = False
+
+    def visit_for_stmt(self: "Interpreter", stmt: For) -> None:
+        self._execute(stmt.initializer)
+        while self._is_truthy(
+            self._evaluate(stmt.condition) if stmt.condition else True
+        ):
+            try:
+                self._execute(stmt.body)
+            except Break:
+                break
+            except Continue:
+                continue
+            finally:
+                if stmt.increment:
+                    self._evaluate(stmt.increment)
+
+    def visit_function_stmt(self: "Interpreter", stmt: FunctionStmt) -> None:
+        fn_name: str = stmt.name.lexeme
+        function: Function = Function(fn_name, stmt.function, self._environment)
+        self._environment.define(stmt.name.lexeme, function)
+
+    def visit_if_stmt(self: "Interpreter", stmt: If) -> None:
+        if self._is_truthy(self._evaluate(stmt.condition)) and stmt.then_branch:
+            self._execute(stmt.then_branch)
+        elif stmt.else_branch:
+            self._execute(stmt.else_branch)
+
+    def visit_multi_var_stmt(self: "Interpreter", stmt: MultiVar) -> None:
+        for variable in stmt.variables:
+            variable.accept(self)
+
+    def visit_return_stmt(self: "Interpreter", stmt: ReturnStmt) -> None:
+        value: object = None
+        if stmt.value:
+            value = self._evaluate(stmt.value)
+        raise Return(value)
+
+    def visit_var_stmt(self: "Interpreter", stmt: Var) -> None:
+        value: object = Uninitialized()
+        self._environment.define(stmt.name.lexeme, value)
+
+        if stmt.initializer is not None:
+            value = self._evaluate(stmt.initializer)
+            self._environment.assign(stmt.name, value)
+
+    def visit_while_stmt(self: "Interpreter", stmt: While) -> None:
+        while self._is_truthy(self._evaluate(stmt.condition)):
+            try:
+                self._execute(stmt.body)
+            except Break:
+                break
+            except Continue:
+                continue

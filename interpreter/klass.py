1187ef962b31bf707a44e0db577facb2e22958ec
Tsubasa <scientistjishan@gmail.com>
2023-06-24 23:57:21 +0530

    Added support for class.

---
+++ interpreter/klass.py
@@ -0,0 +1,36 @@
+from typing import TYPE_CHECKING, Final, Optional
+
+from .callable import Callable
+from .function import Function
+from .instance import Instance
+
+if TYPE_CHECKING:
+    from .interpreter import Interpreter
+
+
+class Class(Callable):
+    def __init__(self: "Class", name: str, methods: dict[str, Function]):
+        self.name: Final[str] = name
+        self.methods: Final[dict[str, Function]] = methods
+
+    def __repr__(self: "Class") -> str:
+        return f"<class {self.name}>"
+
+    def __str__(self: "Class") -> str:
+        return self.__repr__()
+
+    def arity(self: "Class") -> int:
+        intializer: Optional[Function] = self.find_method(self.name)
+        return intializer.arity() if intializer else 0
+
+    def call(
+        self: "Class", interpreter: "Interpreter", arguments: list[object]
+    ) -> object:
+        instance: Instance = Instance(self)
+        initializer: Optional[Function] = self.find_method(self.name)
+        if initializer:
+            initializer.bind(instance).call(interpreter, arguments)
+        return instance
+
+    def find_method(self: "Class", name: str) -> Optional[Function]:
+        return self.methods.get(name)
